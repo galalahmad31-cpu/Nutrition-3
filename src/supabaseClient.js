582e12e146b60b933b3fd94197f16efd4db6d656
galalahmad31-cpu <galalahmad31@gmail.com>
2026-05-05 05:24:41 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // استبدل الكلام اللي بالعربي بالبيانات من Supabase
-const supabaseUrl = 'رابط_مشروعك_هنا'
-const supabaseKey = 'المفتاح_بتاعك_هنا'
+const supabaseUrl = 'svnqppvwptbhxcurxipa'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2bnFwcHZ3cHRiaHhjdXJ4aXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyOTE0MTUsImV4cCI6MjA5Mjg2NzQxNX0.riPKzpjgI41EwVCI6KKtbNuyl8NCsRGOwZP_sRNr9Kw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
