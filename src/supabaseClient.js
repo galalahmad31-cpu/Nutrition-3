d37fcac51283a68ba481ea79da5541b043fea9e9
galalahmad31-cpu <galalahmad31@gmail.com>
2026-05-08 00:11:22 +0300

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// استبدل الكلام اللي بالعربي بالبيانات من Supabase
-const supabaseUrl = 'svnqppvwptbhxcurxipa'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2bnFwcHZ3cHRiaHhjdXJ4aXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyOTE0MTUsImV4cCI6MjA5Mjg2NzQxNX0.riPKzpjgI41EwVCI6KKtbNuyl8NCsRGOwZP_sRNr9Kw'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
